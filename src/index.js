f2204d412de580ec9ac23afeb99416b35e18b6b2
kir-22 <kirill89kireew@gmail.com>
2018-02-28 22:15:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // my solution
+  var array = [];
     if (str.length % 2 != 0) {
         return false;
     }
     str = str.split('');
-    var array = [];
+    
     for (var i = 0; i < str.length; i++) {
         for (var j = 0; j < bracketsConfig.length; j++) {
             var n=array.length-1;
