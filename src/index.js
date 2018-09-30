927fd7cd8c4b888484cc822398d19425e5c97bf8
kir-22 <kirill89kireew@gmail.com>
2018-09-30 21:04:31 +0300

    additional-5: Es6

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-    if (str.length % 2 != 0) {
+    const array = [];
+    if (str.length % 2 !== 0) {
         return false;
     }
     str = str.split('');
-    var array = [];
-    for (var i = 0; i < str.length; i++) {
-        for (var j = 0; j < bracketsConfig.length; j++) {
-            var n=array.length-1;
-            if ((array[n] == bracketsConfig[j][0]) && (str[i] == bracketsConfig[j][1]))
+
+    for (let i = 0, length=str.length; i < length; i++) {
+        for (let j = 0, len=bracketsConfig.length; j < len; j++) {
+            let n=array.length-1;
+            if ((array[n] === bracketsConfig[j][0]) && (str[i] === bracketsConfig[j][1]))
                 array.pop();
-            else if (str[i] == bracketsConfig[j][0])
+            else if (str[i] === bracketsConfig[j][0])
                 array.push(str[i]);
         }
     }
-    return array.length == 0;
+    return array.length === 0;
+    
+    
 }
